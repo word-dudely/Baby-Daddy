2505d6c9709b0b40d1e96d75ed9f7079e337dc8b
word-dudely <ryanrockers@gmail.com>
2015-08-31 06:46:56 -0700

    updated the main script file

--- BabyDaddy.py
+++ BabyDaddy.py
@@ -1,5 +1,17 @@
 # -** coding: utf-8 -*-
-import pygame
-import codecs
+import pygame, codecs, math, sys
+from pygame.locals import *
 
-print ("Your are the Daddy")
+screen = pygame.display.set_mode((1000,800))
+pygame.display.set_caption('Baby Daddy')
+clock = pygame.time.Clock()
+BLACK = (0,0,0)
+
+while 1:
+    clock.tick(30)
+    for event in pygame.event.get():
+        if not hasattr(event, 'key'): continue
+        if event.key == K_ESCAPE: sys.exit(0)
+    bg = pygame.image.load('images/logo.jpg')
+    screen.blit(bg, (0,0))
+    pygame.display.flip()
